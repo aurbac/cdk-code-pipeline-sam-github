108374467823df24a5feb34c0a3d74e1bf4d8b92
EC2 Default User <ec2-user@ip-172-31-1-206.ec2.internal>
2024-04-21 19:17:25 +0000

    Initial commit

---
+++ test/cdk-code-pipeline-sam-github.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCodePipelineSamGithub from '../lib/cdk-code-pipeline-sam-github-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-code-pipeline-sam-github-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCodePipelineSamGithub.CdkCodePipelineSamGithubStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
